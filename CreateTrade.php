ef174c2d27dac8d54e4396b6703149adcfc12606
theogofen <origofen@gmail.com>
2019-10-24 21:00:52 +0300

    Fix CreateOrder bug and closeOrder

--- CreateTrade.php
+++ CreateTrade.php
@@ -8,44 +8,58 @@ $config = include('config.php');
 function is_buy($type) {
     return $type == 'Buy' ? 1:0;
 }
-if (isset($argv[1])) {
+
+$tradeSymbols = array('XBTUSD', 'XBT7D_U105', 'ADAZ19', 'BCHZ19', 'EOSZ19','ETHUSD', 'LTCZ19', 'TRXZ19', 'XRPZ19');
+$tradeTypes = array('Buy', 'Sell');
+
+if (isset($argv[1]) and in_array($argv[1], $tradeSymbols)) {
     $symbol = $argv[1];
 }
 else {
+    echo 'Error: '.$argv[1].' is not a valid symbol'."\n";
     exit();
 }
-if (isset($argv[2])) {
+if (isset($argv[2]) and 0.001 < $argv[2] and $argv[2] < 100) {
     $intervalPercentage = floatval($argv[2]) / 100;
 }
 else {
+    echo 'Error: '.$argv[2]. ' is not a valid interval'."\n";
     exit();
 }
-if (isset($argv[3])) {
-    $target = $argv[3];
+if (isset($argv[3]) and 0.001 < $argv[3] and $argv[3] < 100) {
+    $targetPercent = floatval($argv[3]) / 100;
 }
 else {
+    echo 'Error: '.$argv[3]. ' is not a valid target'."\n";
     exit();
 }
-if (isset($argv[4])) {
+if (isset($argv[4]) and in_array($argv[4], $tradeTypes)) {
     $type = $argv[4];
 }
 else {
+    echo 'Error: '.$argv[4]. ' is not a valid trade type'."\n";
     exit();
 }
 if (isset($argv[5])) {
     $amount = $argv[5];
 }
 else {
+    echo 'Error: failed to get amount'."\n";
     exit();
 }
+$order = False;
 $bitmex = new BitMex($config['key'],$config['secret']);
 $bitmex->setLeverage($config['leverage'], $symbol);
 $result = $bitmex->getTicker($symbol);
 $lastPrice = $result["last"];
 $interval = $lastPrice * $intervalPercentage;
-$order = $bitmex->createOrder($symbol, "Market",$type, null, $amount);
+$target = $lastPrice + $lastPrice * $targetPercent;
+do {
+    $order = $bitmex->createOrder($symbol, "Market",$type, null, $amount);
+} while ($order == False);
 if (!is_array($order) or is_array($order) and empty($order)) {
     echo "ERROR: Order Failure\n";
+    var_dump($order);
     exit();
 }
 $orderId = $order['orderID'];
@@ -83,8 +97,11 @@ while(1) {
         }
     }
     if ($tmpLastPrice > $close and !is_buy($type) or $tmpLastPrice < $close and is_buy($type)) {
+        $close = False;
         $log->info("Position reached it's close price, thus Closing.", ['Close Price'=>$tmpLastPrice]);
-        $close = $bitmex->closePosition($symbol, null);
+        do {
+            $close = $bitmex->closePosition($symbol, null);
+        } while ($close == False);
         $log->info("Trade has closed successfully", ['info'=>$close]);
         break;
     }
