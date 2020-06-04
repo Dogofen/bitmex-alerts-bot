93341e61efd34134276ce1146201e8d11423c899
ogofen <origofen@gmail.com>
2020-06-04 17:46:13 +0300

    better idle time

--- index.php
+++ index.php
@@ -7,28 +7,6 @@ $logPath =  getcwd().'/index.log';
 $log = create_logger($logPath);
 $oldMessagesIds = false;
 
-function verify_trade_exists($argv) {
-    $tradeTypes = array('Buy', 'Sell');
-    if (isset($argv[3]) and in_array($argv[3], $tradeTypes)) {
-    $type = $argv[3];
-    }
-    if (isset($argv[5]) and $argv[5] == "reverse_pos") {
-        $pid = $argv[6];
-        $currentFileName = 'reverse_'.($type).'_'.$pid;
-        if (file_exists($currentFileName)) {
-            return false;
-        }
-    }
-    if (isset($argv[5]) and $argv[5] == "with_id") {
-        $pid = $argv[6];
-        $currentFileName = 'with_id_'.$pid;
-        if (file_exists($currentFileName)) {
-            return false;
-        }
-    }
-    return true;
-}
-
 $log->info("Bot index has started", ['info'=>'start']);
 $gmailClient = new GmailClient();
 
@@ -48,7 +26,8 @@ while(1) {
     }
 
     if(empty($newMessagesIds)) {
-        sleep(60);
+        shell_exec('touch bot_idle');
+        sleep(15);
     }
 
     else {
@@ -64,7 +43,6 @@ while(1) {
             $res = $gmailClient->isMessageAlert($msg);
             $res == True ? shell_exec('php CreateTrade.php '.$params.' > /dev/null 2>&1 &') : "Not alert message\n";
             $log->info('Command was sent to Trader '.$command, ['msg id'=>$msgId]);
-            $log->info('Command was not sent. '.$params, ['msgId'=>$msgId]);
             $gmailClient->populateMessageId($msgId);
         }
     }
